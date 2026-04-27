5c48636ac65e5fe77130060235d1c37fcf4406a0
minseong <vvvvv72@pusan.ac.kr>
2026-04-27 13:48:41 +0900

    step 3

--- mynumber.c
+++ mynumber.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 int main(){
+	printf("Odd Numbers\n");
 	for (int i=1; i<=100; i++){
 		if (i%2!=0){
+			printf("%d ",i);
+		}
+	}
+	printf("\n");
+	printf("Even Numbers\n");
+	for(int i=1; i<=100; i++){
+		if (i%2==0){
 			printf("%d ",i);
 		}
 	}
