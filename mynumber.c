5249d547c9abc567ce192f55cc73700ef04f312d
minseong <vvvvv72@pusan.ac.kr>
2026-04-27 19:16:36 +0900

    step 4

--- mynumber.c
+++ mynumber.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+#include <stdbool.h>
+bool isprime(int n){
+	for (int i=2; i<n; i++){
+		if (n%i==0){
+			return false;
+		}	
+	}
+	return true;
+}
 int main(){
 	printf("Odd Numbers\n");
 	for (int i=1; i<=100; i++){
@@ -10,6 +19,13 @@ int main(){
 	printf("Even Numbers\n");
 	for(int i=1; i<=100; i++){
 		if (i%2==0){
+			printf("%d ",i);
+		}
+	}
+	printf("\n");
+	printf("Prime Numbers\n");
+	for (int i=1; i<=100; i++){
+		if (isprime(i)==true){
 			printf("%d ",i);
 		}
 	}
